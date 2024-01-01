e063671a9d44e99e27ab5a3f1c112c97f7a1ab95
Crxa <carakawidi07@gmail.com>
2024-01-01 21:54:41 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Livewire\Counter;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::view('/', 'welcome');
+
+Route::get('/counter', Counter::class)->middleware(['guest'])->name('counter');
+
+
+Route::view('dashboard', 'dashboard')
+    ->middleware(['auth', 'verified'])
+    ->name('dashboard');
+
+Route::view('profile', 'profile')
+    ->middleware(['auth'])
+    ->name('profile');
+
+require __DIR__ . '/auth.php';
